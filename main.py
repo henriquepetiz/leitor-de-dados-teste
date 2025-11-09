55dfd1fc25b0bedf2b69d3556fde6a9b0cbc43e9
Henrique Petiz <heishenrique@gmail.com>
2025-11-09 20:00:30 +0000

    organização do código e melhora na visualização

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ f = open("sinalizacao.csv")
 
 ###identificar data mais antiga
 
-antiga = ''
-primeira = True
+antiga = '' ### variavel para armazenar a data mais antiga
+primeira = True ### variavel para identificar a primeira linha
+ausente = [] ### identificar dados ausentes
+interesse = [] ## identificar dados de interesse
 for line in f:
   if primeira:
     primeira = False
@@ -15,7 +17,16 @@ for line in f:
   lista = line.strip().split(';')   ###cria lista 
   if antiga == '' or antiga > lista[4]:
     antiga = lista[4]
-  print(lista)
+  if lista[13].strip() == '' or lista[14].strip() == '':
+    ausente.append(lista)
+  if lista[13].strip() != '' or lista[14].strip() != '':
+    interesse.append(lista)
+    
+
 
 ##Sinalliza a data mais antiga no arquivo
 print(f"Data da primeira implementacao: {antiga}")
+print() ## quebra de linha para melhor visualizacao
+print(f"Dados sem latitude ou longitude:  {ausente}")
+print() ## quebra de linha para melhor visualizacao
+print(f"Dados com latitude ou longitude:  {interesse}")
